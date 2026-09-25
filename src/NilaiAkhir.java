dda5a74e03a1a4e9ac855abff0c9b9bedcffc835
Theresia Angel Simbolon <2581015@unai.edu>
2026-09-25 09:32:11 +0700

    Menambahkan kontanta bobot penilaian

--- src/NilaiAkhir.java
+++ src/NilaiAkhir.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class NilaiAkhir {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+// Konstanta bobot penilaian
+final double BOBOT_PRAKTIKUM = 0,3;
+final double BOBOT_TUGAS = 0,2;
+final double BOBOT_MID = 0,2;
+final double BOBOT_FINAL = 0,3;
