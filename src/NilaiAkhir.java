cae00c662c91cbfb80127e1ce33daf2541abdb13
Theresia Angel Simbolon <2581015@unai.edu>
2026-09-25 09:44:56 +0700

    Menambahkan input Nilai dan Perbaikan Huruf dan tanda

--- src/NilaiAkhir.java
+++ src/NilaiAkhir.java
@@ -5,7 +5,22 @@ public class NilaiAkhir {
 
         Scanner scanner = new Scanner(System.in);
 // Konstanta bobot penilaian
-final double BOBOT_PRAKTIKUM = 0,3;
-final double BOBOT_TUGAS = 0,2;
-final double BOBOT_MID = 0,2;
-final double BOBOT_FINAL = 0,3;
+        final double BOBOT_PRAKTIKUM = 0.3;
+        final double BOBOT_TUGAS = 0.2;
+        final double BOBOT_MID = 0.2;
+        final double BOBOT_FINAL = 0.3;
+
+// Input Nilai
+        System.out.print("Nilai praktikum :");
+        double praktikum = scanner.nextDouble();
+
+        System.out.print("Nilai tugas     :");
+        double NilaiTugas = scanner.nextDouble();
+
+        System.out.print("Nilai MID      :");
+        double NilaiMID = scanner.nextDouble();
+
+        System.out.print("Nilai final     :");
+        double NilaiFinal = scanner.nextDouble();
+    }
+}
